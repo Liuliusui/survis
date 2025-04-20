4cd0083f3dce05d90930839007bcbc00f30c384c
Liuliusui <2830885260@qq.com>
2025-04-20 19:52:57 +0800

    upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -2,10 +2,10 @@
     "bai2023analytics": {
         "author": "Bai, Ruibin and Chen, Zhi-Long and Kendall, Graham",
         "journal": "International Journal of Production Research",
+        "keywords": "type:Analytics, Artificial Intelligence, Data Science, Decision Making, Operations Research, Optimization, Supply Chain Management",
         "number": "1",
         "pages": "1--3",
         "publisher": "Taylor \\& Francis",
-        "title": "Analytics and machine learning in scheduling and routing research",
         "type": "misc",
         "volume": "61",
         "year": "2023"
@@ -13,6 +13,7 @@
     "chen2020data": {
         "author": "Chen, Xinan and Bai, Ruibin and Qu, Rong and Dong, Haibo and Chen, Jianjun",
         "booktitle": "2020 IEEE congress on evolutionary computation (CEC)",
+        "keywords": "type:Genetic programming, Heuristic algorithms, Optimization, Data-driven methods, Seaport container terminal truck dispatching",
         "organization": "Ieee",
         "pages": "1--8",
         "title": "A data-driven genetic programming heuristic for real-world dynamic seaport container terminal truck dispatching",
@@ -22,6 +23,7 @@
     "ghiani2024integrating": {
         "author": "Ghiani, Gianpaolo and Solazzo, Gianluca and Elia, Gianluca and others",
         "journal": "Algorithms",
+        "keywords": "type:Large Language Models, Optimization, Decision Making, Semi-Structured Problems, Case Study",
         "title": "Integrating Large Language Models and Optimization in Semi-Structured Decision Making: Methodology and a Case Study",
         "type": "article",
         "volume": "17",
@@ -30,6 +32,7 @@
     "huang2024can": {
         "author": "Huang, Zhehui and Shi, Guangyao and Sukhatme, Gaurav S",
         "journal": "arXiv preprint arXiv:2403.10795",
+        "keywords": "type:Large Language Models, Robot Routing, Optimization, AI",
         "title": "Can Large Language Models Solve Robot Routing?",
         "type": "article",
         "year": "2024"
@@ -37,6 +40,7 @@
     "liu2023algorithm": {
         "author": "Liu, Fei and Tong, Xialiang and Yuan, Mingxuan and Zhang, Qingfu",
         "journal": "arXiv preprint arXiv:2311.15249",
+        "keywords": "type:Algorithm Evolution, Large Language Model, Optimization, AI",
         "title": "Algorithm evolution using large language model",
         "type": "article",
         "year": "2023"
@@ -44,6 +48,7 @@
     "liu2024evolution": {
         "author": "Liu, Fei and Tong, Xialiang and Yuan, Mingxuan and Lin, Xi and Luo, Fu and Wang, Zhenkun and Lu, Zhichao and Zhang, Qingfu",
         "journal": "arXiv preprint arXiv:2401.02051",
+        "keywords": "type:Heuristics, Algorithm Design, Large Language Model, Optimization, AI",
         "title": "Evolution of heuristics: Towards efficient automatic algorithm design using large language model",
         "type": "article",
         "year": "2024"
@@ -51,6 +56,7 @@
     "liu2024llm4ad": {
         "author": "Liu, Fei and Zhang, Rui and Xie, Zhuoliang and Sun, Rui and Li, Kai and Lin, Xi and Wang, Zhenkun and Lu, Zhichao and Zhang, Qingfu",
         "journal": "arXiv preprint arXiv:2412.17287",
+        "keywords": "type:Algorithm Design, Large Language Model, Optimization, AI",
         "title": "Llm4ad: A platform for algorithm design with large language model",
         "type": "article",
         "year": "2024"
@@ -58,6 +64,7 @@
     "romera2024mathematical": {
         "author": "Romera-Paredes, Bernardino and Barekatain, Mohammadamin and Novikov, Alexander and Balog, Matej and Kumar, M Pawan and Dupont, Emilien and Ruiz, Francisco JR and Ellenberg, Jordan S and Wang, Pengming and Fawzi, Omar and others",
         "journal": "Nature",
+        "keywords": "type:Large Language Models, Program Search, Mathematical Discoveries, AI",
         "number": "7995",
         "pages": "468--475",
         "publisher": "Nature Publishing Group UK London",
@@ -69,6 +76,7 @@
     "yao2025multi": {
         "author": "Yao, Shunyu and Liu, Fei and Lin, Xi and Lu, Zhichao and Wang, Zhenkun and Zhang, Qingfu",
         "booktitle": "Proceedings of the AAAI Conference on Artificial Intelligence",
+        "keywords": "type:Multi-objective Evolution, Heuristic Algorithms, Large Language Model, Optimization, AI",
         "number": "25",
         "pages": "27144--27152",
         "title": "Multi-objective evolution of heuristic using large language model",
@@ -79,6 +87,7 @@
     "zhang2024understanding": {
         "author": "Zhang, Rui and Liu, Fei and Lin, Xi and Wang, Zhenkun and Lu, Zhichao and Zhang, Qingfu",
         "booktitle": "International Conference on Parallel Problem Solving from Nature",
+        "keywords": "type:Automated Heuristic Design, Large Language Models, Evolutionary Search, Optimization, AI",
         "organization": "Springer",
         "pages": "185--202",
         "title": "Understanding the importance of evolutionary search in automated heuristic design with large language models",
